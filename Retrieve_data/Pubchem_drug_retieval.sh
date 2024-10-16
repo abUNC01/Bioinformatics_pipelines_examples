6d5c6181a8970c1bf7a4f46fe1727737ff0cb4b9
abUNC01 <132288383+abUNC01@users.noreply.github.com>
2024-10-16 16:14:46 -0400

    changed the 'drug_query' to be the input from the CLI

--- Retrieve_data/Pubchem_drug_retieval.sh
+++ Retrieve_data/Pubchem_drug_retieval.sh
@@ -4,7 +4,10 @@
 API_ENDPOINT="https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/"
 
 # Query the target drug
-drug_query="trametinib" 
+# Old within script 
+## drug_query="trametinib" 
+# New drug query from CLI after calling script
+drug_query=$1
 
 FILE_OUTPUT_FORMAT="json"
 
@@ -12,7 +15,12 @@ FILE_OUTPUT_FORMAT="json"
 API_Call="${API_ENDPOINT}${drug_query}/${FILE_OUTPUT_FORMAT}"
 
 # CURL Request to get the call and retrieve the json data of the target drug
+##  Headers for 
 curl -X GET "${API_CALL}" \
   -H 'Accept: application/json' \
   -H 'Content-Type: application/json' \
   -o "molecular_data_${drug_query}.${FILE_OUTPUT_FORMAT}"
+
+
+
+# Based this script on the pubchem API, PUG. Found here: https://pubchem.ncbi.nlm.nih.gov/docs/pug-rest
