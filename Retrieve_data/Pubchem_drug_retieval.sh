ba4173c2bc8035901b10a9806b0a68353abac699
abUNC01 <132288383+abUNC01@users.noreply.github.com>
2024-10-18 11:27:51 -0400

    fixed my comment scheme. added and echo to print the name of the retrived file data

--- Retrieve_data/Pubchem_drug_retieval.sh
+++ Retrieve_data/Pubchem_drug_retieval.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-# Set the endpoint for the pubchem url
+#> Set the endpoint for the pubchem url
 API_ENDPOINT="https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/"
 
-# Query the target drug
-# Old within script 
+#> Query the target drug
+#> Old within script 
 ## drug_query="trametinib" 
-# New drug query from CLI after calling script
+#> New drug query from CLI after calling script
 drug_query=$1
 
 FILE_OUTPUT_FORMAT="json"
 
-# The api request
+#> The api request
 API_Call="${API_ENDPOINT}${drug_query}/${FILE_OUTPUT_FORMAT}"
 
-# CURL Request to get the call and retrieve the json data of the target drug
-##  Headers for 
+#> CURL Request to get the call and retrieve the json data of the target drug
+##>  Headers for curl to denote I want the data back in json format
 curl -X GET "${API_CALL}" \
   -H 'Accept: application/json' \
   -H 'Content-Type: application/json' \
   -o "molecular_data_${drug_query}.${FILE_OUTPUT_FORMAT}"
 
-
+#> print to shell what the data return file is
+echo "Molecular data for ${1} is save to molecular_data_${1}.${FILE_OUTPUT_FORMAT}" 
 
 # Based this script on the pubchem API, PUG. Found here: https://pubchem.ncbi.nlm.nih.gov/docs/pug-rest
